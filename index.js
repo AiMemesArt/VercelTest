b00baee5114f6e5db07a02d1f829350e1226c416
shamoon1997 <sshamoon83@gmail.com>
2023-02-09 08:55:42 +0500

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+
+const app = express();
+
+// Enable body Parser 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// TODO: disable CORS in production
+app.use(cors({
+   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar', 'Authorization'],
+}));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+app.listen(port, () => console.log(`Server started on port ${port}
+   Meme Generator oWo is ON`))
---
+++ readme.md
@@ -0,0 +1,27 @@
+# OpenAI Image Generator
+
+This is a simple image generator built with Node.js and Express that uses [OpenAI's Dall-E models](https://beta.openai.com/docs/guides/images) to generate images with the ability to add certain theme to the image .
+
+<img src="public/img/img.JPG" width="500">
+
+## Usage
+
+Rename the `example.env` file to `.env`.
+
+Generate an API KEY at [OpenAI](https://beta.openai.com/) and add it to the `.env` file.
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+Run server
+
+```bash
+npm start
+```
+
+
+
+
