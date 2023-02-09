f0f047618a429ad0de077d0aa23a8eeb20ccfc93
shamoon1997 <sshamoon83@gmail.com>
2023-02-09 09:05:18 +0500

    done

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,58 +1,45 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
-
 
 const openai = new OpenAIApi(configuration);
 
-const generateImage = async ( req ,res) => {
+const generateImage = async (req, res) => {
+  const { prompt, size } = req.body;
 
+  const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
+  try {
+    const response = await openai.createImage({
+      prompt: 'make me a funny meme about ' + prompt,
 
-    const {prompt,size} = req.body;
+      n: 1,
+      size: imageSize,
+    });
 
- 
+    console.log(req.body);
+    const imageUrl = response.data.data[0].url;
 
-  
-    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
-
-
-   try {
-     const response = await openai.createImage({
-        prompt:"make me a funny meme about " + prompt,
-     
-        n: 1,
-        size: imageSize,
-      });
-
-    
-        console.log(req.body)
-     const imageUrl = response.data.data[0].url
- 
-
-     res.status(200).json({
-        success: true,
-        data: imageUrl,
-      
-    
-     });
-
-
-   } catch (error) {
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
     if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
 
     res.status(400).json({
-        success: false,
-        error: 'The Image Could Not Be Generated'
-     });
-   }
+      success: false,
+      error: 'The Image Could Not Be Generated',
+    });
+  }
 };
 
 module.exports = { generateImage };
